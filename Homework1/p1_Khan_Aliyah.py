9133bf1649b726299d2045e451ea8995b2c798fa
Aliyah Khan <AliyahKhan2020@fau.edu>
2026-09-09 20:31:20 -0400

    updating hw

--- Homework1/p1_Khan_Aliyah.py
+++ Homework1/p1_Khan_Aliyah.py
@@ -7,10 +7,12 @@ def quadratic(a, b, c):
     a = float(a)
     b = float(b)
     c = float(c)
+    #calculating under radical
     radical = (b**2)-(4*a*c)
     if radical < 0:
         print("no real solutions")
     else:
+        #calculating entire formula if possible
         plus = ((-b) + (math.sqrt(radical)))/(2*a)
         minus = ((-b) - (math.sqrt(radical)))/(2*a)
         if radical == 0:
@@ -28,24 +30,22 @@ while True:
     a, b, c, radical = quadratic(a, b, c)
     print()
     
-    #choose the graph domain
     if radical < 0:
-        xopt = -b/(2*a)
+        #picking domain for no real solutions
+        xopt = (-b)/(2*a)
         xmin = xopt - 5
         xmax = xopt + 5
     else:
+        #picking domain for one or two solutions
         x1 = ((-b) - math.sqrt(radical))/(2*a)
         x2 = ((-b) + math.sqrt(radical))/(2*a)
         xmin = min(x1, x2) - 2
         xmax = max(x1, x2) + 2
     
-    #make 150 x values
     x = np.linspace(xmin, xmax, 150)
-    
-    #calculate y values
     y = a*x**2 + b*x + c
     
-    #make the graph
+    #graphing
     plt.figure()
     plt.plot(x, y)
     plt.axhline(0)
