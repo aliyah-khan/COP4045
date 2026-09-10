21ee7e77c0f4109a173a97987be4be78d689eae7
Aliyah Khan <AliyahKhan2020@fau.edu>
2026-09-09 23:12:34 -0400

    updating hw

--- Homework1/p3_Khan_Aliyah.py
+++ Homework1/p3_Khan_Aliyah.py
@@ -1,9 +1,9 @@
+#3
 def find_dup_str(s, n):
     #checking every possible first substring
     for i in range(len(s) - n + 1):
         sub = s[i:i+n]
-        
-        #start after the first substring to avoid overlap
+        #starting after the first substring to avoid overlap
         for j in range(i + n, len(s) - n + 1):
             if sub == s[j:j+n]:
                 return sub
@@ -11,8 +11,10 @@ def find_dup_str(s, n):
 
 
 def find_max_dup(s):
-    #starting with the longest possible substring
+    #starting with the longest possible substring and going down
     for n in range(len(s), 0, -1):
+        #calling function above to see if a duplicate of the
+        #same length exists
         result = find_dup_str(s, n)
         if result != "":
             return result
@@ -21,5 +23,5 @@ def find_max_dup(s):
 s = input("Enter a string: ")
 n = int(input("Enter substring length: "))
 
-print(find_dup_str(s, n))
-print(find_max_dup(s))
+print("\nDuplicated substring of length ", n, ": ", find_dup_str(s, n), sep = "")
+print("Longest duplicated substring:", find_max_dup(s))
--- Homework1/p4_Khan_Aliyah.py
+++ Homework1/p4_Khan_Aliyah.py
@@ -1,38 +1,38 @@
+#4
 import math
 import matplotlib.pyplot as plt
 
-#q4
 def plot_function(fun_str, domain, ns):
     xmin, xmax = domain
     
     #creating a list of x values by calculating
     #average distance between each point
-    xs = []
+    xlist = []
     split = (xmax - xmin) / (ns - 1)
     for i in range(ns):
         x = xmin + i * split
-        xs.append(x)
+        xlist.append(x)
     
     #calculating y values by inputting x
     #into the user's formula
-    ys = []
-    for x in xs:
+    ylist = []
+    for x in xlist:
         y = eval(fun_str)
-        ys.append(y)
+        ylist.append(y)
         
     #printing the table of points
     print("\n       x          y")
     print("-----------------------")
     
-    for i in range(min(12, len(xs))):
-        print(f"{xs[i]:10.4f} {ys[i]:10.4f}")
-    if len(xs) > 12:
+    for i in range(min(12, len(xlist))):
+        print(f"{xlist[i]:10.4f} {ylist[i]:10.4f}")
+    if len(xlist) > 12:
         print("----------------------- (rest of the table not shown)")
     else:
         print("-----------------------")
     
     #making graph
-    plt.plot(xs, ys, "o-")
+    plt.plot(xlist, ylist, "o-")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.title(fun_str)
